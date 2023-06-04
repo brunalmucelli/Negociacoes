ccfc2672c7cfcc8a96862e03f4af541ccc2ff4cf
brunalmucelli <brunalmucelli@gmail.com>
2023-06-04 15:53:44 -0300

    finalização curso 2 - parte 2

---
+++ app/views/view.ts
@@ -0,0 +1,14 @@
+export abstract class View<T>{
+    protected elemento: HTMLElement;
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+
+    abstract template(model: T): string;
+
+    update(model: T): void {
+        const template = this.template(model);
+        console.log(template);
+        this.elemento.innerHTML = template;
+    }
+}
